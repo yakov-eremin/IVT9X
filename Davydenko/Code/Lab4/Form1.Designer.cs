f33b7b7faf2c87333118efbf78ac9d2505f02af5
I_D4C_I <98944264+I-D4C-I@users.noreply.github.com>
2022-03-30 00:46:58 +0700

    Игра реализована
    Создан интерфейс и реализован функционал классов

--- Davydenko/Code/Lab4/Form1.Designer.cs
+++ Davydenko/Code/Lab4/Form1.Designer.cs
@@ -28,21 +28,191 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label_points_1 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label_points_2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label_points_3 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label_points = new System.Windows.Forms.Label();
+            this.button_wheel = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox_question = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(10, 220);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(88, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(10, 248);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(88, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Подтвердить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label_points_1
+            // 
+            this.label_points_1.AutoSize = true;
+            this.label_points_1.Location = new System.Drawing.Point(69, 205);
+            this.label_points_1.Name = "label_points_1";
+            this.label_points_1.Size = new System.Drawing.Size(13, 13);
+            this.label_points_1.TabIndex = 3;
+            this.label_points_1.Text = "0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(130, 220);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(88, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(130, 246);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(88, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Подтвердить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label_points_2
+            // 
+            this.label_points_2.AutoSize = true;
+            this.label_points_2.Location = new System.Drawing.Point(194, 205);
+            this.label_points_2.Name = "label_points_2";
+            this.label_points_2.Size = new System.Drawing.Size(13, 13);
+            this.label_points_2.TabIndex = 3;
+            this.label_points_2.Text = "0";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(250, 220);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(88, 20);
+            this.textBox3.TabIndex = 1;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(250, 246);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(88, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Подтвердить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label_points_3
+            // 
+            this.label_points_3.AutoSize = true;
+            this.label_points_3.Location = new System.Drawing.Point(307, 205);
+            this.label_points_3.Name = "label_points_3";
+            this.label_points_3.Size = new System.Drawing.Size(13, 13);
+            this.label_points_3.TabIndex = 3;
+            this.label_points_3.Text = "0";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(280, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Барабан";
+            // 
+            // label_points
+            // 
+            this.label_points.AutoSize = true;
+            this.label_points.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label_points.Location = new System.Drawing.Point(290, 60);
+            this.label_points.Name = "label_points";
+            this.label_points.Size = new System.Drawing.Size(18, 20);
+            this.label_points.TabIndex = 3;
+            this.label_points.Text = "0";
+            // 
+            // button_wheel
+            // 
+            this.button_wheel.Location = new System.Drawing.Point(280, 82);
+            this.button_wheel.Name = "button_wheel";
+            this.button_wheel.Size = new System.Drawing.Size(75, 23);
+            this.button_wheel.TabIndex = 2;
+            this.button_wheel.Text = "Крутить";
+            this.button_wheel.UseVisualStyleBackColor = true;
+            this.button_wheel.Click += new System.EventHandler(this.button_wheel_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 13);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Вопрос";
+            // 
+            // textBox_question
+            // 
+            this.textBox_question.Location = new System.Drawing.Point(10, 25);
+            this.textBox_question.Multiline = true;
+            this.textBox_question.Name = "textBox_question";
+            this.textBox_question.ReadOnly = true;
+            this.textBox_question.Size = new System.Drawing.Size(264, 103);
+            this.textBox_question.TabIndex = 4;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(396, 277);
+            this.ClientSize = new System.Drawing.Size(369, 276);
+            this.Controls.Add(this.textBox_question);
+            this.Controls.Add(this.label_points);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label_points_3);
+            this.Controls.Add(this.label_points_2);
+            this.Controls.Add(this.label_points_1);
+            this.Controls.Add(this.button_wheel);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label_points_1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label_points_2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label_points_3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label_points;
+        private System.Windows.Forms.Button button_wheel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox_question;
     }
 }
 
--- Davydenko/Code/Lab4/Form1.cs
+++ Davydenko/Code/Lab4/Form1.cs
@@ -5,7 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace Lab4
@@ -16,11 +16,183 @@ namespace Lab4
         {
             InitializeComponent();
         }
-
+        Library _library;
+        HiddenWord _hiddenWord;
+        Player[] _players = new Player[3];
+        int[] _points = new int[] { 100, 200, 300, 400, 500 };
+        Random random = new Random();
+        Label[] _answerletters;
+        int _playerTurn;
+        int _currentpoints;
+        
         private void Form1_Load(object sender, EventArgs e)
         {
-
+            _library = new Library("AnswerQuestions.xml");
+            Start();
+        }
+        private void Start()
+        {
+            string _answer;
+            string _question;
+            Library.GetRandomPuzzle(out _question, out _answer);
+            textBox_question.Text = _question;
+            _hiddenWord = new HiddenWord(_answer);
+            _players = new Player[3];
+            _answerletters = new Label[_answer.Length];
+            int _x = 10;
+            for (int i = 0; i < _answerletters.Length; i++)
+            {
+                _answerletters[i] = new Label();
+                _answerletters[i].AutoSize = false;
+                _answerletters[i].Width = 15;
+                _answerletters[i].Text = "_";
+                _answerletters[i].Location = new Point(_x, 150);
+                _x += 25;
+                Controls.Add(_answerletters[i]);
+            }
+            _x = 10;
+            for (int i = 0; i < _players.Length; i++)
+            {
+                _players[i] = new Player();
+                _players[i].Name = "Игрок " + (i + 1);
+                _players[i].ResetPoints();
+                Label _label = new Label();
+                _label.Text = _players[i].Name;
+                _label.Location = new Point(_x, 205);
+                _x += 120;
+                Controls.Add(_label);
+            }
+            label_points_1.Text = _players[0].Points.ToString();
+            label_points_2.Text = _players[1].Points.ToString();
+            label_points_3.Text = _players[2].Points.ToString();
+            _playerTurn = 0;
+            NextTurn();
         }
 
+        private void NextTurn()
+        {
+            _playerTurn++;
+            if (_playerTurn > 3)
+                _playerTurn = 1;
+            if (_playerTurn == 1)
+            {
+                button1.Enabled = true;
+                button2.Enabled = false;
+                button3.Enabled = false;
+            }
+            if (_playerTurn == 2)
+            {
+                button1.Enabled = false;
+                button2.Enabled = true;
+                button3.Enabled = false;
+            }
+            if (_playerTurn == 3)
+            {
+                button1.Enabled = false;
+                button2.Enabled = false;
+                button3.Enabled = true;
+            }
+            button_wheel.Enabled = true;
+        }
+
+        private void OpenLetters(List<int> index)
+        {
+            char _letter = _hiddenWord.Word[index[0]];
+            for (int i = 0; i < index.Count; i++)
+            {
+                _answerletters[index[i]].Text = _letter.ToString();
+            }
+        }
+        
+        private void ShowWord()
+        {
+            for(int i = 0; i < _answerletters.Length; i++)
+            {
+                _answerletters[i].Text = _hiddenWord.Word[i].ToString();
+            }
+        }
+
+        private void button_wheel_Click(object sender, EventArgs e)
+        {
+            _currentpoints = _points[random.Next(0, _points.Length)];
+            label_points.Text = _currentpoints.ToString();
+            button_wheel.Enabled = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<int> _index = new List<int>();            
+            if (textBox1.Text.Length == 1)
+            {
+                if(_hiddenWord.TryGuessLetter(Convert.ToChar(textBox1.Text), _index) == true)
+                {
+                    _players[0].IncreasePoints(_currentpoints);
+                    label_points_1.Text = _players[0].Points.ToString();
+                    OpenLetters(_index);
+                }
+            }
+            else
+            {
+                if (_hiddenWord.TryGuessWord(textBox1.Text))
+                {
+                    _players[0].IncreasePoints(_currentpoints);
+                    label_points_1.Text = _players[0].Points.ToString();
+                    ShowWord();                    
+                }
+            }
+            textBox1.Clear();
+            NextTurn();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            List<int> _index = new List<int>();
+            if (textBox2.Text.Length == 1)
+            {
+                if (_hiddenWord.TryGuessLetter(Convert.ToChar(textBox2.Text), _index) == true)
+                {
+                    _players[1].IncreasePoints(_currentpoints);
+                    label_points_2.Text = _players[1].Points.ToString();
+                    OpenLetters(_index);
+                }
+            }
+            else
+            {
+                if (_hiddenWord.TryGuessWord(textBox2.Text))
+                {
+                    _players[1].IncreasePoints(_currentpoints);
+                    label_points_2.Text = _players[1].Points.ToString();
+                    ShowWord();
+                    
+                }
+            }
+            textBox2.Clear();
+            NextTurn();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            List<int> _index = new List<int>();
+            if (textBox3.Text.Length == 1)
+            {
+                if (_hiddenWord.TryGuessLetter(Convert.ToChar(textBox3.Text), _index) == true)
+                {
+                    _players[2].IncreasePoints(_currentpoints);
+                    label_points_3.Text = _players[2].Points.ToString();
+                    OpenLetters(_index);
+                }
+            }
+            else
+            {
+                if (_hiddenWord.TryGuessWord(textBox3.Text))
+                {
+                    _players[2].IncreasePoints(_currentpoints);
+                    label_points_3.Text = _players[2].Points.ToString();
+                    ShowWord();
+                }
+            }
+            textBox3.Clear();
+            NextTurn();
+        }
     }
 }
