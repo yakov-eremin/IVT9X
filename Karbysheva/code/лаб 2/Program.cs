01942e8abeb2805eaaf2eb642ec6dffde0c9aa4f
KatyaKarbysheva <100127195+KatyaKarbysheva@users.noreply.github.com>
2022-04-04 18:42:20 +0700

    Добавлен код тестов и код программы

---
+++ Karbysheva/code/лаб 2/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace KProject
+{
+    public class SomeClass
+    {
+        //Работа с классом
+        public double Var1 { get; set; }
+        public int Var2 { get; set; }
+
+        public SomeClass(double var1, int var2)
+        {
+            Var1 = var1;
+            Var2 = var2;
+        }
+
+        public SomeClass Best(SomeClass some)
+        {
+            if (((int)this.Var1 + this.Var2) > ((int)some.Var1 + some.Var2))
+            {
+                return this;
+            }
+            else
+            {
+                return some;
+            }
+        }
+
+        //Работа с числами
+        public static int SingOfNumber(string number)
+        {
+            if (double.TryParse(number, out double res))
+            {
+                if (Convert.ToDouble(number) < 0)
+                    return -1;
+                if (Convert.ToDouble(number) > 0)
+                    return 1;
+                if (Convert.ToDouble(number) == 0)
+                    return 0;
+                return -1;
+            }
+            else
+            {
+                throw new ArgumentNullException("Не число.");
+            }
+        }
+        //Работа со строками
+        public static void EachLetter(string String, List<char> Symbol, List<int> Count)
+        {
+            if (String == null)
+            {
+                throw new ArgumentNullException("Срока пустая.");
+            }
+            for (int i = 0; i < String.Length; i++)
+            {
+                if (!Symbol.Contains(String[i]))
+                {
+                    Symbol.Add(String[i]);
+                    Count.Add(1);
+                }
+                else
+                {
+                    Count[Symbol.IndexOf(String[i])]++;
+                }
+            }
+        }
+
+        //Работа с массивами
+        public static void BubbleSort(double[] array, int mode)
+        {
+            if (mode == 1)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 0; j < array.Length - 1; j++)
+                    {
+                        if (array[j] > array[j + 1])
+                        {
+                            double temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+                        }
+                    }
+                }
+            }
+            if (mode == -1)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    for (int j = 0; j < array.Length - 1; j++)
+                    {
+                        if (array[j] < array[j + 1])
+                        {
+                            double temp = array[j];
+                            array[j] = array[j + 1];
+                            array[j + 1] = temp;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+    
---
+++ Karbysheva/code/лаб 2/SomeClassTests.cs
@@ -0,0 +1,131 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using KProject;
+using System.Collections.Generic;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class SomeClassTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+    [TestClass]
+    public class WorkWithNumbersTests
+    {
+        [TestMethod]
+        public void SingOfNumberPositive()
+        {
+            string number = "1";
+            int actual = SomeClass.SingOfNumber(number);
+            Assert.AreEqual(1, actual);
+        }
+
+        [TestMethod]
+        public void SingOfNumberNegative()
+        {
+            string number = "-1";
+            int actual = SomeClass.SingOfNumber(number);
+            Assert.AreEqual(-1, actual);
+        }
+        [TestMethod]
+        public void SingOfNumberZero()
+        {
+            string number = "0";
+            int actual = SomeClass.SingOfNumber(number);
+            Assert.AreEqual(0, actual);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void SingOfNumberNullExcepstion()
+        {
+            string str = "";
+            int actual = SomeClass.SingOfNumber(str);
+        }
+    }
+    [TestClass]
+    public class WorkWithSringsTests
+    {
+        [TestMethod]
+        public void EachLetterCount()
+        {
+            string str = "asdfs";
+            List<char> list = new List<char>();
+            List<int> listint = new List<int>();
+
+            List<int> expected = new List<int> { 1, 2, 1, 1 };
+
+            SomeClass.EachLetter(str,list,listint);
+            for (int i = 0; i < listint.Count; i++)
+            {
+                Assert.AreEqual(expected[i],listint[i]);
+            }            
+        }
+    }
+    [TestClass]
+    public class WorkWithArrayTests
+    {
+        [TestMethod]
+        public void BubbleSortPositiveDouble()
+        {
+            double[] array = { 3.5, 4.7, 3.1, 9.5, 6.2, 0.6 };
+            int mode = 1;
+            SomeClass.BubbleSort(array, mode);
+            double[] expected = { 0.6, 3.1, 3.5, 4.7, 6.2, 9.5};
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.AreEqual(expected[i], array[i]);
+            }
+        }
+        [TestMethod]
+        public void BubbleSortNegativeDouble()
+        {
+            double[] array = { -3.5, -4.7, -3.1, -9.5, -6.2, -0.6 };
+            int mode = -1;
+            SomeClass.BubbleSort(array, mode);
+            double[] expected = { -0.6, -3.1, -3.5, -4.7, -6.2, -9.5 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.AreEqual(expected[i], array[i]);
+            }
+        }
+
+        [TestMethod]
+        public void BubbleSortMixDouble()
+        {
+            double[] array = { 3.5, -4.7, 3.1, -9.5, -6.2, 0.6 };
+            int mode = -1;
+            SomeClass.BubbleSort(array, mode);
+            double[] expected = { 3.5, 3.1, 0.6, -4.7, -6.2, -9.5 };
+            for (int i = 0; i < array.Length; i++)
+            {
+                Assert.AreEqual(expected[i], array[i]);
+            }
+        }
+    }
+    [TestClass]
+    public class WorkWithClassTests
+    {
+
+        SomeClass someObject;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            someObject = new SomeClass(7.4, 2);
+        }
+         
+        [TestMethod]
+        public void BestWithPositive()
+        {
+            SomeClass some = new SomeClass(9.1, 1);
+            SomeClass actual = some.Best(someObject);
+            Assert.AreEqual(some,actual);
+        }
+        
+    }
+}
