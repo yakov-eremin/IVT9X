e3c6595dd7eb8846b4b3f8ee2e1e96e8b9a19c27
Damodigy <43088109+Damodigy@users.noreply.github.com>
2022-04-04 18:02:04 +0700

    Delete admin panel
    for curious users
    
    Co-Authored-By: ArtyomMaz <102156232+ArtyomMaz@users.noreply.github.com>

--- Reut/Project/habar/AdminPanel/get_state.php
+++
@@ -1,8 +0,0 @@
-<?php
-    //Retrieve the data from our text file.
-    $fileContents = file_get_contents('settings.txt');
-    //Convert the JSON string back into an array.
-    $decoded = json_decode($fileContents, true);
-    
-    echo  json_encode($decoded)   ;
-?>
