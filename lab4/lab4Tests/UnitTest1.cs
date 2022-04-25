a94bf8309be59722494aee8b2d69284d628acb50
Ceebot <mihailceeb8@mail.ru>
2022-04-25 20:56:54 +0700

    Пустой проект с пустым тестом

---
+++ lab4/lab4Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+using lab4;
+
+namespace lab4Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void lab4Test1()
+        {
+            
+        }
+    }
+}
