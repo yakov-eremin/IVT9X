5864c9c302bc5a5e4f4174dc3242dd1ecad5f9fe
kratok111 <kratok111@yandex.ru>
2022-05-01 17:47:11 +0700

    Добавление комментариев к методу isSity

--- Code/Lab_4/Lab_4/Lab_4/SityGame.cs
+++ Code/Lab_4/Lab_4/Lab_4/SityGame.cs
@@ -8,10 +8,21 @@ namespace Lab_4
 {
     public class SityGame
     {
+        /*!
+         
+            /brief Метод, проверяет есть ли город в списке городов
+         
+            \param [in] v Название города
+        
+            \param [out] bool Есть или Нет
+        */
+
         public bool isSity(string v)
         {
             //todo Позже доделать весь функционал 
             return true;
         }
+
+
     }
 }
