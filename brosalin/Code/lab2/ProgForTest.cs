b1c9694bb1fa781760ca834214c3099fe9c7f907
br-nikita <nikita20011402@mail.ru>
2022-05-13 00:41:17 +0700

    add lab2

---
+++ brosalin/Code/lab2/ProgForTest.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgForTest
+{
+    public class ProgForTest
+    {
+        public double Rad(int x)
+        {
+            return Math.Sqrt(x);
+        }
+
+        public int Sq(int x)
+        {
+            return x * x;
+        }
+
+    }
+
+    public class Mas
+    {
+
+        public int SearchMax_div_2(int[] mas)// поиск максимального чётного числа
+        {
+            int max = 2;
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                if (mas[i] % 2 == 0)
+                    if (mas[i] > max)
+                        max = mas[i];
+            }
+          
+            return max;
+        }
+
+        public int Sum(int[] mas) // сумма всех эл-тов массива
+        {
+            int s=0;
+            for (int i = 0; i < mas.Length; i++)
+                s = s + mas[i];
+            return s;
+        }
+
+        public int most_repeated(int[] mas)//Поиск эллемента, который встречается в массиве чаще, чем остальные
+        {
+            int a=0;
+            int count=0;
+            int elem=0;
+            for (int i = 0; i < mas.Length; i++)
+            {
+                a = 0;
+                for (int j = 0; j < mas.Length; j++)
+                {
+                    if (mas[i] == mas[j])
+                        a++;
+                }
+                if (a > count)
+                {
+                    count = a;
+                    elem = mas[i];
+                }
+            }
+            return elem;
+        }
+    }
+
+    public class Ugol
+    {
+        private float degree;
+        private float min;
+        private float sec;
+        public void Init(float d, float m, float s)
+        {
+            degree = d;
+            min = m;
+            sec = s;
+        }
+
+        public float getdeg()
+        {
+            return degree;
+        }
+        public float getmin()
+        {
+            return min;
+        }
+        public float getsec()
+        {
+            return sec;
+        }
+        public float round()//округление до градусов
+        {
+            if (sec >= 30)
+            {
+                min++;
+            }
+            sec = 0;
+            if (min >= 30)
+            {
+                degree++;
+            }
+            min = 0;
+            return degree;
+        }
+
+        public Ugol add(Ugol a, Ugol b)//сложение углов
+        {
+            Ugol c;
+            c = new Ugol();
+            c.degree = a.degree + b.degree;
+            c.min = a.min + b.min;
+            c.sec = a.sec + b.sec;
+            if (c.sec >= 60)
+            {
+                c.min++;
+                c.sec = c.sec - 60;
+            }
+            if (c.min >= 60)
+            {
+                c.degree++;
+                c.min = c.min - 60;
+            }
+            return c;
+        }
+    }
+
+    public class stringclass //поиск слов
+    {
+        public String findstr(string a, string b)
+        {
+            string s="";
+            bool f;
+            int k, count;
+            count = b.Length;
+            f = a.Contains(b);
+            if (f == true)
+            {
+                s = a;
+                k = s.IndexOf(b);
+                s = s.Insert(k, "[");
+                s = s.Insert(k + 1 + count, "]");
+            }
+            return s;
+        }
+    }
+}
+
---
+++ brosalin/Code/lab2/UnitTests.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ProgForTest
+{
+    [TestClass]
+    public class MyCalcTests
+    {
+        [TestMethod]
+        public void TestRad()
+        {
+            int a = 25;
+            int expected = 5;
+            ProgForTest c = new ProgForTest();
+            double actual = c.Rad(a);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestSq()
+        {
+            int a = 5;
+            int expected = 25;
+            ProgForTest c = new ProgForTest();
+            double actual = c.Sq(a);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    [TestClass]
+    public class MasTests
+    {
+        [TestMethod]
+        public void Test_most_repeated()
+        {
+            int[] a = { 1, 2, 3, 4, 5, 6, 5, 3, 2, 7, 3, 4, 7, 2, 3 };
+            int expected = 3;
+            Mas m = new Mas();
+            int actual = m.most_repeated(a);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Sum()
+        {
+            int[] a = { 1, 2, 3, 4, 5, 6, 5, 3, 2, 7, 3, 4, 7, 2, 3 };
+            int expected = 57;
+
+            Mas m = new Mas();
+            int actual = m.Sum(a);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_SearchMax_div_2()
+        {
+            int[] a = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int expected = 10;
+
+            Mas m = new Mas();
+            int actual = m.SearchMax_div_2(a);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+
+    [TestClass]
+    public class UgolTests
+    {
+        [TestMethod]
+        public void Test_round()//тестирование округления угла до градусов
+        {
+            Ugol a = new Ugol();
+            a.Init(10, 29, 40);
+            int expected = 11;
+            float actual = a.round();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Add()//тестирование сложения углов
+        {
+            Ugol a = new Ugol();
+            Ugol b = new Ugol();
+            Ugol c = new Ugol();
+            Ugol d = new Ugol();
+            a.Init(10, 20, 30);
+            b.Init(5, 15, 25);
+            d.Init(15, 35, 55);
+            c = c.add(a, b);
+            float expected = d.getdeg();
+            float actual = c.getdeg();
+            Assert.AreEqual(expected, actual);
+
+            expected = d.getmin();
+            actual = c.getmin();
+            Assert.AreEqual(expected, actual);
+
+            expected = d.getsec();
+            actual = c.getsec();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]//проверка сложения углов с переполнением градусов и минут (градусы и минуты > 60)
+        public void Test_Add_60()
+        {
+            Ugol a = new Ugol();
+            Ugol b = new Ugol();
+            Ugol c = new Ugol();
+            Ugol d = new Ugol();
+            a.Init(10, 40, 20);
+            b.Init(5, 55, 47);
+            d.Init(16, 36, 7);
+            c = c.add(a, b);
+            float expected = d.getdeg();
+            float actual = c.getdeg();
+            Assert.AreEqual(expected, actual);
+
+            expected = d.getmin();
+            actual = c.getmin();
+            Assert.AreEqual(expected, actual);
+
+            expected = d.getsec();
+            actual = c.getsec();
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+
+    [TestClass]
+    public class StringclassTests
+    {
+        [TestMethod]
+        public void Test_findstr()//тест поиска подстроки в строке
+        {
+            string a = "The quick brown fox jumps over the lazy dog";
+            string b = "fox";
+            string expected = "The quick brown [fox] jumps over the lazy dog";
+            stringclass m = new stringclass();
+            string actual = m.findstr(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_findstr_0()//тест поиска несуществующей подстроки в строке
+        {
+            string a = "The quick brown fox jumps over the lazy dog";
+            string b = "cat";
+            string expected = "";
+            stringclass m = new stringclass();
+            string actual = m.findstr(a, b);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
