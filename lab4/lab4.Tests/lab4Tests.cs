fdce170ac27b2cde0036c86f302b8482dcd1f501
Karidzava <danil.gergert@mail.ru>
2022-05-22 23:06:05 +0700

    Лаб 4 Готовый проект

--- lab4/lab4.Tests/lab4Tests.cs
+++ lab4/lab4.Tests/lab4Tests.cs
@@ -10,9 +10,52 @@ namespace lab4.Tests
         }
 
         [Test]
-        public void Test1()
+        public void Test_Create_Class_Calculate()
         {
-            Assert.Pass();
+            Calculate calc = new Calculate();
+            Assert.IsNotNull(calc);
+        }
+
+        [Test]
+        public void Test_Method_dollar_in_rub()
+        {
+            Calculate calc = new Calculate();
+            Assert.AreEqual(6300, calc.dollarinrub(63, 100));
+        }
+
+        [Test]
+        public void Test_Method_rub_in_dollar()
+        {
+            Calculate calc = new Calculate();
+            Assert.AreEqual(100, calc.rubindollar(6300, 63));
+        }
+
+        [Test]
+        public void Test_Method_rub_in_euro()
+        {
+            Calculate calc = new Calculate();
+            Assert.AreEqual(100, calc.rubineuro(7500, 75));
+        }
+
+        [Test]
+        public void Test_Method_euro_in_rub()
+        {
+            Calculate calc = new Calculate();
+            Assert.AreEqual(7500, calc.euroinrub(75, 100));
+        }
+
+        [Test]
+        public void Test_Method_euro_in_dollar()
+        {
+            Calculate calc = new Calculate();
+            Assert.AreEqual(106, calc.euroindollar(1.06, 100));
+        }
+
+        [Test]
+        public void Test_Method_dollar_in_euro()
+        {
+            Calculate calc = new Calculate();
+            Assert.AreEqual(100, calc.dollarineuro(1.06, 106));
         }
     }
 }
--- lab4/lab4/Program.cs
+++ lab4/lab4/Program.cs
@@ -8,11 +8,125 @@ using System.Threading.Tasks;
 
 namespace lab4 
 {
+    public class Calculate
+    {
+        public double dollarinrub(double kursdollar, double amountdollar)
+        {
+            double konvert = kursdollar * amountdollar;
+            return konvert;
+        }
+
+        public double rubindollar(double amountrub, double kursdollar)
+        {
+            double konvert = amountrub / kursdollar;
+            return konvert;
+        }
+
+        public double rubineuro(double amountrub, double kurseuro)
+        {
+            double konvert = amountrub / kurseuro;
+            return konvert;
+        }
+
+        public double euroinrub(double kurseuro, double amounteuro)
+        {
+            double konvert = kurseuro * amounteuro;
+            return konvert;
+        }
+
+        public double euroindollar(double ratio, double amounteuro)
+        {
+            double konvert = ratio * amounteuro;
+            return konvert;
+        }
+
+        public double dollarineuro(double ratio, double amountdollar)
+        {
+            double konvert = amountdollar / ratio;
+            return konvert;
+        }
+    }
     public class Program
     {
         static void Main(string[] args)
         {
-            
+            string command = string.Empty;
+            while (true)
+            {
+                Console.WriteLine("Что вы хотите конвертировать?");
+                Console.WriteLine("1. Конвертировать доллары в рубли");
+                Console.WriteLine("2. Конвертировать рубли в доллары");
+                Console.WriteLine("3. Конвертировать рубли в евро");
+                Console.WriteLine("4. Конвертировать евро в рубли");
+                Console.WriteLine("5. Конвертировать евро в доллары");
+                Console.WriteLine("6. Конвертировать доллары в евро");
+                command = Console.ReadLine();
+                if (command == "1")
+                {
+                    Console.WriteLine("Введите курс доллара: ");
+                    double kursdollar = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите количество долларов: ");
+                    double amountdollar = double.Parse(Console.ReadLine());
+                    Calculate konvert = new Calculate();
+                    double result = konvert.dollarinrub(kursdollar, amountdollar);
+                    Console.WriteLine("Количество рублей = " + result);
+                }
+
+                else if (command == "2")
+                {
+                    Console.WriteLine("Введите курс доллара: ");
+                    double kursdollar = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите количество рублей: ");
+                    double amountrub = double.Parse(Console.ReadLine());
+                    Calculate konvert = new Calculate();
+                    double result = konvert.rubindollar(amountrub, kursdollar);
+                    Console.WriteLine("Количество долларов = " + result);
+                }
+
+                else if (command == "3")
+                {
+                    Console.WriteLine("Введите курс евро: ");
+                    double kurseuro = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите количество рублей: ");
+                    double amountrub = double.Parse(Console.ReadLine());
+                    Calculate konvert = new Calculate();
+                    double result = konvert.rubineuro(amountrub, kurseuro);
+                    Console.WriteLine("Количество евро = " + result);
+                }
+
+                else if (command == "4")
+                {
+                    Console.WriteLine("Введите курс евро: ");
+                    double kurseuro = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите количество евро: ");
+                    double amounteuro = double.Parse(Console.ReadLine());
+                    Calculate konvert = new Calculate();
+                    double result = konvert.euroinrub(kurseuro, amounteuro);
+                    Console.WriteLine("Количество рублей = " + result);
+                }
+
+                else if (command == "5")
+                {
+                    Console.WriteLine("Введите коэффициент отношения доллара к евро: ");
+                    double ratio = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите количество евро: ");
+                    double amounteuro = double.Parse(Console.ReadLine());
+                    Calculate konvert = new Calculate();
+                    double result = konvert.euroindollar(ratio, amounteuro);
+                    Console.WriteLine("Количество долларов = " + result);
+                }
+
+                else if (command == "6")
+                {
+                    Console.WriteLine("Введите коэффициент отношения доллара к евро: ");
+                    double ratio = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Введите количество долларов: ");
+                    double amountdollar = double.Parse(Console.ReadLine());
+                    Calculate konvert = new Calculate();
+                    double result = konvert.dollarineuro(ratio, amountdollar);
+                    Console.WriteLine("Количество евро = " + result);
+                }
+            }
         }
     }
 }
