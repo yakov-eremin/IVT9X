9fb9b696a7c253b4eca59bbde57d58181db17a80
Karidzava <danil.gergert@mail.ru>
2022-05-22 20:13:31 +0700

    Пустой проект с тестом

---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace lab4 
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
