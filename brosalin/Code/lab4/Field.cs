049b926601454841857e01a8cc64392aa75f7729
br-nikita <nikita20011402@mail.ru>
2022-05-23 23:55:46 +0700

    Добавил методы init и Display
    Метод init заполняет будущее поле битвы нулями, метод Display вывод поле на экран

--- brosalin/Code/lab4/Field.cs
+++ brosalin/Code/lab4/Field.cs
@@ -6,9 +6,43 @@ using System.Threading.Tasks;
 
 namespace lab4
 {
+    
     public class MyField
     {
+        private char[,] mas = new char[10, 10];
 
+        public void init() //инициализация поля
+        {
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                {
+                    mas[i, j] = 'o';
+                }
+        }
+
+        public char get(int y, int x)
+        {
+            return mas[y, x];
+        }
+
+        public void Display() //вывод поля
+        {
+            Console.WriteLine("      1   2   3   4   5   6   7   8   9   10");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.Write(i + 1);
+                if (i == 9)
+                    Console.Write("    ");
+                else
+                    Console.Write("     ");
+                for (int j = 0; j < 10; j++)
+                    Console.Write(mas[i, j] + "   ");
+            }
+            Console.WriteLine();
+            Console.WriteLine();
+        }
     }
 
     public class Warships
--- brosalin/Code/lab4/FieldTests.cs
+++ brosalin/Code/lab4/FieldTests.cs
@@ -14,13 +14,33 @@ namespace Field.Tests
             Assert.IsNotNull(field1);
         }
 
+        public void init_Test()
+        {
+            MyField field1 = new MyField();
+            char [,] expected = new char[10,10];
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                    expected[i, j] = 'o';
+            char[,] actual = new char[10, 10];
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                    actual[i, j] = field1.get(i, j);
+            for (int i = 0; i < 10; i++)
+                for (int j = 0; j < 10; j++)
+                    Assert.AreEqual(expected[i,j], actual[i,j]);
+        }
+
+        public void Display_Test()
+        {
+
+        }
     }
 
     [TestClass]
     public class WarshipsTests
     {
         [TestMethod]
-        public void WarshipsCreationTest()
+        public void WarshipsCreation_Test()
         {
             Warships warship1 = new Warships();
             Assert.IsNotNull(warship1);
