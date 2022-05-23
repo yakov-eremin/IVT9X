004ebd8f033dcfed5851b94652e2ff421c4d181b
br-nikita <nikita20011402@mail.ru>
2022-05-23 23:36:27 +0700

    Создал классы MyField и Warships

---
+++ brosalin/Code/lab4/FieldTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using lab4;
+
+namespace Field.Tests
+{
+    [TestClass]
+    public class FieldTests
+    {
+        [TestMethod]
+        public void FieldCreation_Test()
+        {
+            MyField field1 = new MyField();
+            Assert.IsNotNull(field1);
+        }
+
+    }
+
+    [TestClass]
+    public class WarshipsTests
+    {
+        [TestMethod]
+        public void WarshipsCreationTest()
+        {
+            Warships warship1 = new Warships();
+            Assert.IsNotNull(warship1);
+        }
+    }
+}
