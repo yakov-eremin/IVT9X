fe4f6d4135e57bb16bc5f11600cfcd00c90897cb
Alexander Burashov <a.burashov2000@yandex.ru>
2022-05-25 03:53:55 +0700

    Добавлены файлы пустого проекта

---
+++ Burashov/Code/Lab4/Lab4Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Lab4Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Lab4Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("4675cd36-230c-43cd-b21e-1da853a8ee52")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Burashov/Code/Lab4/Lab4Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lab4Tests
+{
+    [TestClass]
+    public class Lab4Tests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
