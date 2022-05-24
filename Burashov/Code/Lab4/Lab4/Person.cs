288fc1aa2606824f1dfb3636ef27893b07e69c37
Alexander Burashov <a.burashov2000@yandex.ru>
2022-05-25 03:58:34 +0700

    Тест на создание объекта класса Person

---
+++ Burashov/Code/Lab4/Lab4/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Lab4
+{
+    public class Person
+    {
+        public string name { get; set; }
+        public DateTime date { get; set; }
+
+        public Person(string name, DateTime date)
+        {
+            this.name = name;
+            this.date = date;
+        }
+    }
+}
--- Burashov/Code/Lab4/Lab4Tests/UnitTest1.cs
+++ Burashov/Code/Lab4/Lab4Tests/UnitTest1.cs
@@ -1,14 +1,16 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Lab4;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Lab4Tests
 {
     [TestClass]
     public class Lab4Tests
     {
-        [TestMethod]
-        public void TestMethod1()
+        [TestInitialize]
+        public void PersonInitTest()
         {
-
+            Person p = new Person("Ivan Ivanov", new DateTime(2000, 1, 1));
         }
     }
 }
