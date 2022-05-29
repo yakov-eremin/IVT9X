cb715457323a7ef124d7a550404e019f54768706
Stanislav <stasons.mail@gmail.com>
2022-05-30 02:13:31 +0700

    Создал класс Number

---
+++ QA_Lab4/UnitTest1.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QA_Lab4_Form; 
+
+
+namespace QA_Lab4_Tests
+{
+    [TestClass]
+    public class NumberTest
+    {
+        [TestMethod]
+        public void NumberCreation()
+        {
+            Number number = new Number();
+            Assert.IsNotNull(number);
+        }
+    }
+}
