ed065660b972a8ae4172339f98e709e31f8632d8
Stanislav <stasons.mail@gmail.com>
2022-05-30 18:44:28 +0700

    Был добавлен класс Number

---
+++ QA_Lab4/Form1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace QA_Lab4_Form
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class Number
+    {
+        
+    }
+}
