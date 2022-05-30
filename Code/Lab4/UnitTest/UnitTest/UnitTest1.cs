bf9f3905a46459fe06cf1765903eaa74fafb17c7
ruppel <e_ruppel_a@mail.ru>
2022-05-30 19:28:06 +0700

    Метод TestStringVopros2 тест не проходит

--- Code/Lab4/UnitTest/UnitTest/UnitTest1.cs
+++ Code/Lab4/UnitTest/UnitTest/UnitTest1.cs
@@ -13,52 +13,90 @@ namespace UnitTest
         [Test]
         public void TestStringVopros1()
         {
-            StreamReader sr = new StreamReader(@"C:\lab4\lab4.txt");
+            StreamReader sr = new StreamReader(@"C:\lab4.txt");
+            string line;
+            CheckString checkstring = new CheckString();
+            string check = checkstring.vopros1("a");
+
         }
         [Test]
         public void TestStringVopros2()
         {
             CheckString checkstring = new CheckString();
+            string check = checkstring.vopros2("c");
+            string x = "Правильно!!! Вы выиграли 2 000 рублей! ";
+            Assert.AreEqual(x, check);
+
         }
         [Test]
         public void TestStringVopros3()
         {
-            StreamReader sr = new StreamReader(@"C:\lab4\lab4.txt");
+            StreamReader sr = new StreamReader(@"C:\lab4.txt");
+            string line;
+            CheckString checkstring = new CheckString();
+            string check = checkstring.vopros3("b");
+
         }
         [Test]
         public void TestStringVopros4()
         {
             CheckString checkstring = new CheckString();
+            string check = checkstring.vopros4("d");
+            string x = "Правильно! Вы выиграли 10 000 рублей! ";
+            Assert.AreEqual(x, check);
+
         }
         [Test]
         public void TestStringVopros5()
         {
-            StreamReader sr = new StreamReader(@"C:\lab4\lab4.txt");
+            StreamReader sr = new StreamReader(@"C:\lab4.txt");
+            string line;
+            CheckString checkstring = new CheckString();
+            string check = checkstring.vopros5("a");
         }
         [Test]
         public void TestStringVopros6()
         {
             CheckString checkstring = new CheckString();
+            string check = checkstring.vopros6("b");
+            string x = "Правильно! Вы выиграли 50 000 рублей! ";
+            Assert.AreEqual(x, check);
         }
         [Test]
         public void TestStringVopros7()
         {
-            StreamReader sr = new StreamReader(@"C:\lab4\lab4.txt");
+            StreamReader sr = new StreamReader(@"C:\lab4.txt");
+            string line;
+            CheckString checkstring = new CheckString();
+            string check = checkstring.vopros7("d");
+
         }
         [Test]
         public void TestStringVopros8()
         {
             CheckString checkstring = new CheckString();
+            string check = checkstring.vopros8("c");
+            string x = "Правильно! Вы выиграли 200 000 рублей! ";
+            Assert.AreEqual(x, check);
+
         }
         [Test]
         public void TestStringVopros9()
         {
-            StreamReader sr = new StreamReader(@"C:\lab4\lab4.txt");
+            StreamReader sr = new StreamReader(@"C:\lab4.txt");
+            string line;
+            CheckString checkstring = new CheckString();
+            string check = checkstring.vopros9("a");
+
         }
         [Test]
         public void TestStringVopros10()
         {
             CheckString checkstring = new CheckString();
+            string check = checkstring.vopros10("b");
+            string x = "Поздравляем!!! Вы выиграли 1 000 000 рублей! ";
+            Assert.AreEqual(x, check);
+
         }
     }
 }
--- Code/Lab4/lab4/Program.cs
+++ Code/Lab4/lab4/Program.cs
@@ -1,20 +1,413 @@
-﻿using NUnit.Framework;
-using lab4;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.IO;
 
-namespace UnitTest
+namespace lab4
 {
-    public class Tests
+    public class CheckString
     {
-        [SetUp]
-        public void Setup()
+        StreamReader sr = new StreamReader(@"C:\lab4.txt");
+        string line;
+        public string vopros1(string otvet)
         {
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (otvet == "a")
+                {
+                    string[] text = line.Split('*');
+                    return text[1];
+                }
+                else if (otvet == "b")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "c")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "d")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+            }
+            return "";
         }
 
-        [Test]
-        public void TestStringVopros1()
+        public string vopros2(string otvet)
         {
-            CheckString checkstring = new CheckString();
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (otvet == "a")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "b")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "c")
+                {
+                    string[] text = line.Split('*');
+                    return text[2];
+                }
+                else if (otvet == "d")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+            }
+            return "";
+        }
+
+        public string vopros3(string otvet)
+        {
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (otvet == "a")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "b")
+                {
+                    string[] text = line.Split('*');
+                    return text[3];
+                }
+                else if (otvet == "c")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "d")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+            }
+            return "";
+        }
+
+        public string vopros4(string otvet)
+        {
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (otvet == "a")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "b")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "c")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "d")
+                {
+                    string[] text = line.Split('*');
+                    return text[4];
+                }
+            }
+            return "";
+        }
+        public string vopros5(string otvet)
+        {
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (otvet == "a")
+                {
+                    string[] text = line.Split('*');
+                    return text[5];
+                }
+                else if (otvet == "b")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "c")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "d")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+            }
+            return "";
+        }
+
+        public string vopros6(string otvet)
+        {
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (otvet == "a")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "b")
+                {
+                    string[] text = line.Split('*');
+                    return text[6];
+                }
+                else if (otvet == "c")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "d")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+            }
+            return "";
+        }
+
+        public string vopros7(string otvet)
+        {
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (otvet == "a")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "b")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "c")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "d")
+                {
+                    string[] text = line.Split('*');
+                    return text[7];
+                }
+            }
+            return "";
+        }
+
+        public string vopros8(string otvet)
+        {
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (otvet == "a")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "b")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "c")
+                {
+                    string[] text = line.Split('*');
+                    return text[8];
+                }
+                else if (otvet == "d")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+            }
+            return "";
+        }
+
+        public string vopros9(string otvet)
+        {
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (otvet == "a")
+                {
+                    string[] text = line.Split('*');
+                    return text[9];
+                }
+                else if (otvet == "b")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "c")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "d")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+            }
+            return "";
+        }
+
+        public string vopros10(string otvet)
+        {
+            while ((line = sr.ReadLine()) != null)
+            {
+                if (otvet == "a")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "b")
+                {
+                    string[] text = line.Split('*');
+                    return text[10];
+                }
+                else if (otvet == "c")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+                else if (otvet == "d")
+                {
+                    string[] text = line.Split('*');
+                    return text[0];
+                }
+            }
+            return "";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string otvet = string.Empty;
+            string name = "";
+            Console.Write("Введите ваше имя: ");
+            name = Console.ReadLine();
+            Console.Write("Здравствуйте, " + name + "! Вас приветствует игра “Кто хочет стать миллионером?”. \nНаши правила просты: 10 вопросов могут принести 1 млн. руб.,\nЖелаем удачи, игра началась!!! ");
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.Write("\nВопрос 1: Дели, столица какого государства?\na - Индия\nb - Китай\nc - Япония\nd - Россия\n\n");
+                Console.Write(name + ", введите букву с Вашим ответом: ");
+                otvet = Console.ReadLine();
+                CheckString checkstring = new CheckString();
+                string x = checkstring.vopros1(otvet);
+                Console.WriteLine(x);
+                break;
+            }
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.Write("\nВопрос 2: Под каким названием известна единица с последующими ста нулями?\na - Мегатрон\nb - Гигабит\nc - Гугол\nd - Наномоль\n\n");
+                Console.Write(name + ", введите букву с Вашим ответом: ");
+                otvet = Console.ReadLine();
+                CheckString checkstring = new CheckString();
+                string x = checkstring.vopros2(otvet);
+                Console.WriteLine(x);
+                break;
+            }
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.Write("\nВопрос 3: Какое насекомое вызвало короткое замыкание в ранней версии вычислительной машины, тем самым породив термин \n“компьютерный баг”?\na - Таракан\nb - Мотылёк\nc - Муха\nd - Японский хрущик\n\n");
+                Console.Write(name + ", введите букву с Вашим ответом: ");
+                otvet = Console.ReadLine();
+                CheckString checkstring = new CheckString();
+                string x = checkstring.vopros3(otvet);
+                Console.WriteLine(x);
+                break;
+            }
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.Write("\nВопрос 4: Какой химический элемент составляет более половины массы тела человека?\na - Углерод\nb - Кальций\nc - Железо\nd - Кислород\n\n");
+                Console.Write(name + ", введите букву с Вашим ответом: ");
+                otvet = Console.ReadLine();
+                CheckString checkstring = new CheckString();
+                string x = checkstring.vopros4(otvet);
+                Console.WriteLine(x);
+                break;
+            }
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.Write("\nВопрос 5: Какую часть тела также называют “атлант”?\na - Шейный позвонок\nb - Головной мозг\nc - Шестая пара ребёр\nd - Часть плеча\n\n");
+                Console.Write(name + ", введите букву с Вашим ответом: ");
+                otvet = Console.ReadLine();
+                CheckString checkstring = new CheckString();
+                string x = checkstring.vopros5(otvet);
+                Console.WriteLine(x);
+                break;
+            }
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.Write("\nВопрос 6: Сколько кубиков в кубике Рубика?\na - 24\nb - 26\nc - 28\nd - 30\n\n");
+                Console.Write(name + ", введите букву с Вашим ответом: ");
+                otvet = Console.ReadLine();
+                CheckString checkstring = new CheckString();
+                string x = checkstring.vopros6(otvet);
+                Console.WriteLine(x);
+                break;
+            }
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.Write("\nВопрос 7: Какого цвета крайнее правое кольцо в олимпийской символике?\na - Синее\nb - Жёлтое\nc - Зелёное\nd - Красное\n\n");
+                Console.Write(name + ", введите букву с Вашим ответом: ");
+                otvet = Console.ReadLine();
+                CheckString checkstring = new CheckString();
+                string x = checkstring.vopros7(otvet);
+                Console.WriteLine(x);
+                break;
+            }
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.Write("\nВопрос 8: Что изображено на заднем плане картины Леонардо да Винчи “Мона Лиза”?\na - Драпировка\nb - Город\nc - Пейзаж\nd - Стадо овец\n\n");
+                Console.Write(name + ", введите букву с Вашим ответом: ");
+                otvet = Console.ReadLine();
+                CheckString checkstring = new CheckString();
+                string x = checkstring.vopros8(otvet);
+                Console.WriteLine(x);
+                break;
+            }
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.Write("\nВопрос 9: Какая картина Малевича находится в Русском музее?\na - Красный квадрат\nb - Чёрный квадрат\nc - Белый квадрат\nd - “Точильщик”\n\n");
+                Console.Write(name + ", введите букву с Вашим ответом: ");
+                otvet = Console.ReadLine();
+                CheckString checkstring = new CheckString();
+                string x = checkstring.vopros9(otvet);
+                Console.WriteLine(x);
+                break;
+            }
+            while (true)
+            {
+                Console.WriteLine("");
+                Console.Write("\nВопрос 10: Как называется самая глубокая точка поверхности Земли, находящаяся на дне Марианской впадины?\na - Филиппинская плита\nb - Бездна Челленджера\nc - Кермадек\nd - Чёрное Логово\n\n");
+                Console.Write(name + ", введите букву с Вашим ответом: ");
+                otvet = Console.ReadLine();
+                CheckString checkstring = new CheckString();
+                string x = checkstring.vopros10(otvet);
+                Console.WriteLine(x);
+                break;
+            }
         }
     }
 }
