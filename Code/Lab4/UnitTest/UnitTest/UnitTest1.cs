46bae5fd1042d74d88712e49732f6641064dff55
ruppel <e_ruppel_a@mail.ru>
2022-05-30 18:12:18 +0700

    Пустой проект с тестом

---
+++ Code/Lab4/UnitTest/UnitTest/UnitTest1.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using lab4;
+using System.IO;
+
+namespace UnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestStringVopros1()
+        {
+            CheckString checkstring = new CheckString();
+        }
+    }
+}
