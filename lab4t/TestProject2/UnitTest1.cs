ef9c16fcaff26e736015bb322993d42b428f49fa
karidzava <79240522+karidzava@users.noreply.github.com>
2022-06-05 11:19:56 +0700

    Добавил тест для проверки метода перевода рублей в доллары

--- lab4t/TestProject2/UnitTest1.cs
+++ lab4t/TestProject2/UnitTest1.cs
@@ -23,5 +23,12 @@ namespace TestProject2
             Calculate calc = new Calculate();
             Assert.AreEqual(6300, calc.dollarinrub(63, 100));
         }
+
+        [Test]
+        public void Test_Method_rub_in_dollar()
+        {
+            Calculate calc = new Calculate();
+            Assert.AreEqual(100, calc.rubindollar(6300, 63));
+        }
     }
 }
