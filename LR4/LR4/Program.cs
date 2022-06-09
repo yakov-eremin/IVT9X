f29d093d8b9219d07f74f5e48d601018693348fb
soldat852369~ <soldatov.e11@gmil.com>
2022-06-09 23:12:57 +0700

    создание методов класса

--- LR4/LR4/Program.cs
+++ LR4/LR4/Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WhatWordGame
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
+
+namespace LR4
+{
+    public class word_pole
+    {
+        public List<string> words = new List<string>() { "буква", "цифра", "число", "знак", "представление" };
+
+        public int get_score()
+        {
+            return -10;
+        }
+
+        public string set_word()
+        {
+            return "word";
+        }
+
+        public string set_secret(string expected)
+        {
+            return "tt";
+        }
+
+        public void DW(string word)
+        {
+            words.Remove(word);
+        }
+
+        public void fl()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- LR4/TestProject1/UnitTest1.cs
+++ LR4/TestProject1/UnitTest1.cs
@@ -1,5 +1,6 @@
+using LR4;
 using NUnit.Framework;
-
+using WhatWordGame;
 namespace TestProject1
 {
     public class Tests
@@ -10,9 +11,62 @@ namespace TestProject1
         }
 
         [Test]
-        public void Test1()
+        public void TestCreateClass()
         {
-            Assert.Pass();
+             word_pole word1 = new word_pole();
+            Assert.NotNull(word1);
         }
+
+        [Test]
+        public void TestGet_score()
+        {
+
+            int expected = -10;
+
+
+            word_pole word1 = new word_pole();
+            int actual = word1.get_score();
+
+            Assert.AreEqual(expected, actual);
+
+
+        }
+
+
+        [Test]
+        public void TestSetWord()
+        {
+            string expected = "";
+            string actual = "";
+
+            word_pole word1 = new word_pole();
+            actual = word1.set_word();
+
+            Assert.AreNotEqual(expected, actual);
+
+        }
+
+        [Test]
+        public void TestSet_Secret()
+        {
+            string expected = "tt";
+            word_pole word1 = new word_pole();
+            string actual = word1.set_secret(expected);
+            Assert.AreEqual(expected.Length, actual.Length);
+
+        }
+
+        [Test]
+        public void TestDW()
+        {
+
+            word_pole word1 = new word_pole();
+            string word = "буква";
+            word1.DW(word);
+
+            Assert.IsFalse(word1.words.Contains("буква"));
+
+        }
+
     }
 }
