acbd322cfa8f9d77057db5129451984158e15398
WanderlustGG <pagani_zondaf11@mail.ru>
2022-06-09 21:52:03 +0700

    Добавил пустой проект

--- Lab4/Lab4/TestProject1/UnitTest1.cs
+++ Lab4/Lab4/TestProject1/UnitTest1.cs
@@ -11,35 +11,5 @@ namespace TestProject1
         {
         }
 
-        [Test]
-        public void CheckStringTest()
-        {
-            CheckString check = new CheckString();
-            Assert.IsNotNull(check);
-        }
-
-        [Test]
-        public void TestStringBack1()
-        {
-            StreamReader sr = new StreamReader(@"D:\therapist.txt");
-            string line;
-            CheckString checkstring = new CheckString();
-            string check = checkstring.back(20, "1");
-            while ((line = sr.ReadLine()) != null)
-            {
-                string[] text = line.Split('*');
-                Assert.AreEqual(text[1], check);
-            }
-        }
-
-        [Test]
-        public void TestStringBack2()
-        {
-            CheckString checkstring = new CheckString();
-            string check = checkstring.back(9, "3");
-            string x = "Мазь Индометацин на пораженный участок 3-4 раза в сутки. ";
-            Assert.AreEqual(x, check);
-        }
-
     }
 }
