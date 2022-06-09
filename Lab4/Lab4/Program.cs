a42d0f68b55281c6adf14fae1386ac07596535ee
WanderlustGG <pagani_zondaf11@mail.ru>
2022-06-09 22:51:19 +0700

    Создание методов класса

---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace Lab4
+{
+    public class CheckString
+    {
+        public string back()
+        {
+            string b = "qwerty";
+            return b;
+        }
+
+        public string head()
+        {
+            string a = "qwerty";
+            return a;
+        }
+
+        public string leg()
+        {
+            string a = "qwerty132";
+            return a;
+        }
+
+        public string arm()
+        {
+            string a = "qwerty321";
+            return a;
+        }
+
+        public string belly()
+        {
+            string a = "qwerty123";
+            return a;
+        }
+    }
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+
+}
---
+++ Lab4/TestProject1/UnitTest1.cs
@@ -0,0 +1,97 @@
+using Lab4;
+using NUnit.Framework;
+using System.IO;
+
+namespace TestProject1
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void CheckStringTest()
+        {
+            CheckString check = new CheckString();
+            Assert.IsNotNull(check);
+        }
+
+        [Test]
+        public void TestStringBack1()
+        {
+            CheckString check = new CheckString();
+            string a = "qwerty";
+            Assert.AreEqual(a, check.back());
+        }
+
+
+        [Test]
+        public void TestStringBack2()
+        {
+            CheckString check = new CheckString();
+            string a = "qwerty";
+            Assert.AreEqual(a, check.back());
+        }
+        [Test]
+        public void TestStringHead1()
+        {
+            CheckString check = new CheckString();
+            string a = "qwerty";
+            Assert.AreEqual(a, check.head());
+        }
+        [Test]
+        public void TestStringHead2()
+        {
+            CheckString check = new CheckString();
+            string a = "qwerty";
+            Assert.AreEqual(a, check.head());
+        }
+        [Test]
+        public void TestStringLeg1()
+        {
+            CheckString check = new CheckString();
+            string a = "qwerty132";
+            Assert.AreEqual(a, check.leg());
+        }
+        [Test]
+        public void TestStringLeg2()
+        {
+            CheckString check = new CheckString();
+            string a = "qwerty132";
+            Assert.AreEqual(a, check.leg());
+        }
+        [Test]
+        public void TestStringArm1()
+        {
+            CheckString check = new CheckString();
+            string a = "qwerty321";
+            Assert.AreEqual(a, check.arm());
+        }
+        [Test]
+        public void TestStringArm2()
+        {
+            CheckString check = new CheckString();
+            string a = "qwerty321";
+            Assert.AreEqual(a, check.arm());
+        }
+        [Test]
+        public void TestStringBellyrm1()
+        {
+            CheckString check = new CheckString();
+            string a = "qwerty123";
+            Assert.AreEqual(a, check.belly());
+        }
+        [Test]
+        public void TestStringBellyrm2()
+        {
+            CheckString check = new CheckString();
+            string a = "qwerty123";
+            Assert.AreEqual(a, check.belly());
+        }
+
+
+    }
+
+}
