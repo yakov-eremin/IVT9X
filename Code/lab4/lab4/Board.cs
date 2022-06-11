baf91a0101bee4583ca42ed3e183b2c9e4112409
Pechatnov Vladislav <you@example.com>
2022-06-11 22:47:07 +0700

    Написан тест GetSymbol, написан соответствующий метод

--- Code/lab4/lab4/Board.cs
+++ Code/lab4/lab4/Board.cs
@@ -4,6 +4,18 @@ namespace lab4
 {
     public class Board
     {
+        private char[,] board = new char[3, 3];
 
+        public Board()
+        {
+            for (int i = 0; i < 3; i++)
+                for (int j = 0; j < 3; j++)
+                    board[i, j] = '-';
+        }
+
+        public char GetSymbol(int number)
+        {
+            return board[number / 3, number % 3];
+        }
     }
 }
