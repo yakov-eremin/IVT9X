087bedcf657fd396de4c24a8a6ee5cfec5be744f
d0ubtful <devyatkov_kirill00@mail.ru>
2022-06-28 01:23:09 +0700

    Добавлена обработка таймера

--- QA_Lab4/Form1.cs
+++ QA_Lab4/Form1.cs
@@ -23,12 +23,36 @@ namespace Lab4_QA
         {
             timer = new System.Timers.Timer();
             timer.Interval = 1000;
+            timer.Elapsed += Timer_Elapsed;
         }
 
         delegate void UpdateLabel(Label lbl, string value);
         void UpdateDataLabel(Label lbl, string value)
         {
             lbl.Text = value;
+        }
+
+        private void Timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            DateTime currentTime = DateTime.Now;
+            DateTime userTime = dateTimePicker1.Value;
+            if (currentTime.Hour == userTime.Hour && currentTime.Minute == userTime.Minute && currentTime.Second == userTime.Second)
+            {
+                timer.Stop();
+                try
+                {
+                    UpdateLabel upd = UpdateDataLabel;
+                    if (label1.InvokeRequired)
+                        Invoke(upd, label1, "Stop");
+                    SoundPlayer player = new SoundPlayer();
+                    player.SoundLocation = @"D:\Desktop\ring1.wav";
+                    player.PlayLooping();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
         }
 
         private void button_Start_Click(object sender, EventArgs e)
