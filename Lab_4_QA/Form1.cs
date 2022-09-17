2d1e4ce25d71e6cd516b2015a47560bc4ba6e90b
kissweyn <101046413+kissweyn@users.noreply.github.com>
2022-09-17 20:25:05 +0700

    Был написан пустой класс Birthday

---
+++ Lab_4_QA/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace QA_Lab4_Form
+{
+    public partial class MainForm : Form
+    {
+        
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    public class Birthday
+    {        
+
+    }
+}
