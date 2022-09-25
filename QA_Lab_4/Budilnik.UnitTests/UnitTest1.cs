1b3a9066569424b91fbbd65f47c20f95adbf766f
UstinovIV <108265683+UstinovIV@users.noreply.github.com>
2022-09-25 21:58:50 +0700

    Add unit test get interval

--- QA_Lab_4/Budilnik.UnitTests/UnitTest1.cs
+++ QA_Lab_4/Budilnik.UnitTests/UnitTest1.cs
@@ -15,6 +15,12 @@ namespace Budilnik.UnitTests
             Timer timer = new Timer();
             Assert.IsNotNull(timer);
         }
+        [TestMethod]
+        public void Test_get_interval()
+        {
+            System.Windows.Forms.Timer timer = new System.Windows.Forms.Timer();
+            timer.Interval = Form1.Get_Interval(timer);
+        }
     }
 }
 
