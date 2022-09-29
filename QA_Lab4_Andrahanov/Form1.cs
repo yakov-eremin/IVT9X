525cdd8247abeba18faf68f850cc6e2b5bc1b7d2
Spavnvar <108121529+Spavnvar@users.noreply.github.com>
2022-09-29 22:06:25 +0700

    Добавлен метод создания поля и переменные для хранения данных по игре

--- QA_Lab4_Andrahanov/Form1.cs
+++ QA_Lab4_Andrahanov/Form1.cs
@@ -12,9 +12,18 @@ namespace Lab_4_QA
 {
     public partial class Form1 : Form
     {
+        public int[,] map = new int[4, 4];
+        private Tile[,] tiles = new Tile[4, 4];
+        private int score = 0;
+
         public Form1()
         {
+            CreateMap();
             InitializeComponent();
+        }
+
+        private void CreateMap()
+        {
         }
     }
 }
