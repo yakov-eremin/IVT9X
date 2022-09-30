92350eaf63bd42df1c568a6fea4f3b47aecaca3f
NovikovDN <darkusstudio@gmail.com>
2022-09-30 23:21:40 +0700

    Добавил тест для одного метода

--- Code/Lab 4/Generator_tests/Generator_tests/UnitTest1.cs
+++ Code/Lab 4/Generator_tests/Generator_tests/UnitTest1.cs
@@ -6,8 +6,20 @@ namespace Generator_tests
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void getch_test1()
         {
+            string c1 = "Am";
+            string c2 = "G";
+            string c3 = "F";
+            string c4 = "Dm";
+            string nab = "";
+            string expected = "Am G F Dm";
+
+            Accords s = new Accords();
+            string actual = s.getch(c1, c2, c3, c4, nab);
+
+            Assert.AreEqual(expected, actual);
         }
+      
     }
 }
